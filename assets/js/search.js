e203ee760cdcfe3c176dc304bcb8be2756ccddc8
Jamie Turner <j.d.turner1992@aol.com>
2021-03-13 13:06:34 -0500

    code for java search

---
+++ assets/js/search.js
@@ -0,0 +1,19 @@
+var url = " https://api.spoonacular.com/recipes/findByIngredients";
+var apikey = "54f9d0ffffd344e6907e1cb3683f501c";
+
+$(document).ready(function () {
+console.log (1);
+fetch(url + "?ingredients=apples,flour&apiKey=" + apikey)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+     console.log(data)
+
+    })
+
+
+
+}
+)
