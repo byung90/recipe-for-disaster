f063fbdf0f6e5efd56c5150e2fdc9962d3403ec1
Obi <obiude1002@gmail.com>
2021-03-16 23:01:54 -0400

    updated the page with wine cards, summary and instructions. Also updated the JS #38

--- assets/js/recipe_detail.js
+++ assets/js/recipe_detail.js
@@ -1,13 +1,12 @@
 // Call spoonacular API to get recipe detail. https://spoonacular.com/food-api/docs#Get-Recipe-Information
-let request = new XMLHttpRequest();
-request.open("GET", "https://spoonacular.com/food-api/docs#Get-Recipe-Information");
-request.send();
-request.onload = () => {
-    console.log(request);
-    if (request.status === 200) {
-        console.log(request.response);
-    } else {
-        console.log('error ${request.status} ${request.statusText}')
-    }
-}
+var apikey = "7eba54b90be749259b03f159287e801b";
+
+fetch('https://spoonacular.com/food-api/docs#Get-Recipe-Information')
+    .then(response => {
+        return response.json();
+    })
+    .then(users => {
+        console.log(users);
+    });
+
 
